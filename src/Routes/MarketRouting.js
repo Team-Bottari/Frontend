89c4a5fa2cd5287079c2ae7d8860c7dd499c57f1
kimeunseo58 <alan0221@naver.com>
2023-03-24 22:04:32 +0900

    css 수정  및 market 추가

--- src/Routes/MarketRouting.js
+++ src/Routes/MarketRouting.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Market from "../pages/market/Market";
 import MarketPost from "../pages/market/MarketPost";
 import MarketModify from "../pages/market/MarketModify";
+import MarketInfo from "../pages/market/MarketInfo";
 function MarketRouting() {
   return (
     <Routes>
       <Route path="/Market" element={<Market />} />
       <Route path="/MarketPost" element={<MarketPost />} />
       <Route path="/MarketModify" element={<MarketModify />} />
+      <Route path="/MarketInfo" element={<MarketInfo />} />
     </Routes>
   );
 }
--- src/pages/market/MarketInfo.js
+++ src/pages/market/MarketInfo.js
@@ -1,4 +1,77 @@
-const MarketInfo = () => {
-  return <div></div>;
+import { useLocation } from "react-router-dom";
+import HeaderV1 from "../../components/header/HeaderV1";
+import { useState, useEffect, useRef } from "react";
+const MarketInfo = (props) => {
+  const location = useLocation();
+  const [marketPostInfo, setMarketPostInfo] = useState({
+    title: "",
+    price: "",
+    text: "",
+  });
+  const [userData, setUserData] = useState({
+    id: props.id,
+    name: props.name,
+    credit_rating: props.weight,
+  });
+  const [marketPostImg, setMarketPostImg] = useState([]);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    /*
+    async function getPostInfo() {
+      try {
+        const response = await axios.post(
+          "http://wisixicidi.iptime.org:30000/", 
+          {
+            id: location.state,
+          }
+        );
+        console.log(response);
+        setMarketPostInfo(response.data);
+      } catch (err) {
+        console.log(err);
+        alert("오류가 발생했습니다");
+      }
+    }
+    getPostInfo();
+    async function getPostIMG() {
+      try {
+        const response = await axios.post(
+          "http://wisixicidi.iptime.org:30000/",
+          {
+            id: location.state,
+          },
+          { responseType: "arraybuffer" }
+        );
+        const blob = new Blob([response.data], { type: "image/jpeg" });
+        setMarketPostImg(URL.createObjectURL(blob));
+      } catch (err) {
+        console.log(err);
+        alert("오류가 발생했습니다");
+      }
+    }
+    getPostIMG();*/
+  }, [location.state]);
+
+  return (
+    <div>
+      {
+        //<HeaderV1 ID={location.state} />
+      }
+      <img alt="postImg" className="postImg" src={marketPostImg} />
+      <p>{userData.name}</p>
+      <p>{userData.credit_rating}</p>
+      <div>
+        {marketPostInfo.title}
+        {marketPostInfo.price}
+        {marketPostInfo.text}
+      </div>
+      <div>
+        <button>star</button>
+        <button>채팅</button>
+        <button>신고하기</button>
+      </div>
+    </div>
+  );
 }; //마켓게시글
 export default MarketInfo;
