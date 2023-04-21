f18a6eaa5a1bc1d02d6ef4a3d91d5113c1d7b1de
kimeunseo58 <alan0221@naver.com>
2023-04-21 21:14:15 +0900

    marketInfo update

--- src/pages/market/MarketInfo.js
+++ src/pages/market/MarketInfo.js
@@ -1,13 +1,17 @@
 import { useLocation } from "react-router-dom";
 import HeaderV1 from "../../components/header/HeaderV1";
 import { useState, useEffect, useRef } from "react";
+import "../../CSS/market/MarketInfo.css";
 const MarketInfo = (props) => {
   const location = useLocation();
   const [marketPostInfo, setMarketPostInfo] = useState({
-    title: "",
-    price: "",
-    text: "",
+    title: "기본제목",
+    price: "0",
+    text: "기본내용",
+    discountCheck: false,
+    category: "",
   });
+  const [marketInfoImgFile, setMarketInfoImgFile] = useState([]);
   const [userData, setUserData] = useState({
     id: props.id, //console 확인 후 email인지 id인지 판별
     name: props.name,
@@ -54,17 +58,22 @@ const MarketInfo = (props) => {
   }, [location.state]);
 
   return (
-    <div>
+    <div className="MarketInfo">
       {
         //<HeaderV1 ID={location.state} />
       }
-      <img alt="postImg" className="postImg" src={marketPostImg} />
+      {
+        //       <img alt="postImg" className="postImg" src={marketPostImg} /> //이미지 한개씩인지 여러개 한번에인지 따라서 달라짐.
+      }
       <p>{userData.name}</p>
       <p>{userData.credit_rating}</p>
-      <div>
+      <div className="InfoBody_right">
         {marketPostInfo.title}
-        {marketPostInfo.price}
+        <br />
+        {marketPostInfo.price}원
+        <br />
         {marketPostInfo.text}
+        <br />
       </div>
       <div>
         <button>star</button>
