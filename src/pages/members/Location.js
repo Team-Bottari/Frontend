922c1d071f202ab69b44dd2dbbbb8cc24402a57f
kimeunseo58 <alan0221@naver.com>
2023-08-15 17:36:15 +0900

    위치 정보 업데이트

--- src/pages/members/Location.js
+++ src/pages/members/Location.js
@@ -6,10 +6,9 @@ const Location = () => {
   const [map, setMap] = useState(null);
   const [locatoin, setLocation] = useState({ latitude: 0, longitude: 0 }); //위도, 경도
   const [userLocation, setUserLocation] = useState({ location: "" });
-  const [nearLocation, setNearLocation] = useState([{ location: "" }]);
+  const [nearLocation, setNearLocation] = useState([]);
   const [userRadius, setRadius] = useState(4000);
   let geocoder = new kakao.maps.services.Geocoder(); //사용자 주소 좌표-주소 변환
-
   useEffect(() => {
     const container = document.getElementById("map");
     if (navigator.geolocation) {
@@ -17,22 +16,20 @@ const Location = () => {
     }
     const options = {
       center: new kakao.maps.LatLng(locatoin.latitude, locatoin.longitude),
-
-      center: new kakao.maps.LatLng(35.43586881223756, 126.7018554252071),
       level: 7,
     };
     const kakaoMap = new kakao.maps.Map(container, options);
     setMap(kakaoMap);
-    kakao.maps.load(() => {
-      searchNearbyAddresses();
-    });
   }, []);
-
   const locationAccess = (position: any) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
     setLocation({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
+      latitude: latitude,
+      longitude: longitude,
     });
+
+    searchNearbyAddresses(latitude, longitude);
     let coord = new kakao.maps.LatLng(
       position.coords.latitude,
       position.coords.longitude
@@ -45,11 +42,6 @@ const Location = () => {
       }
     };
     geocoder.coord2Address(coord.getLng(), coord.getLat(), callback); //현재 사용자 주소
-    /*
-    setLocation({
-      latitude: 35.43586881223756,
-      longitude: 126.7018554252071,
-    });*/
   };
   const createCircle = (center) => {
     const circle = new kakao.maps.Circle({
@@ -74,56 +66,61 @@ const Location = () => {
       createCircle(center);
     }
   }, [map, locatoin]);
-
-  //원 안에 해당하는 주소 목록 가져오기
-  const searchNearbyAddresses = (center) => {
+  const searchNearbyAddresses = async (latitude, longitude) => {
     const options = {
-      location: new kakao.maps.LatLng(locatoin.latitude, locatoin.longitude),
+      location: new kakao.maps.LatLng(latitude, longitude),
       radius: userRadius,
       sort: kakao.maps.services.SortBy.DISTANCE,
     };
     const placesService = new kakao.maps.services.Places();
-    placesService.keywordSearch(
-      "지방행정기관",
-      (result, status) => {
-        console.log("결과:", result);
-        if (status === kakao.maps.services.Status.OK) {
-          const centerLatLng = new kakao.maps.LatLng(
-            locatoin.latitude,
-            locatoin.longitude
-          );
-          const radius = userRadius / 1000;
-          const nearbyAddresses = result.filter((item) => {
-            const lat = item.y;
-            const lng = item.x;
-            // 원의 중심과 해당 주소의 거리를 계산합니다.
-            const distance = calculateDistance(
-              centerLatLng.getLat(),
-              centerLatLng.getLng(),
-              lat,
-              lng
-            );
-            // 해당 주소가 원의 반지름 내에 있는지 확인합니다.
-            return distance <= radius;
-          });
-          //const temp = nearbyAddresses.address_name.split[""];
-          console.log("필터링결과:", nearbyAddresses);
-          setNearLocation(nearbyAddresses);
-        } else {
-          console.error("검색에 실패했습니다.");
+    try {
+      const result = await new Promise((resolve, reject) => {
+        placesService.keywordSearch(
+          "지방행정기관",
+          (result, status) => {
+            if (status === kakao.maps.services.Status.OK) {
+              resolve(result);
+            } else {
+              reject(new Error("검색에 실패했습니다."));
+            }
+          },
+          options
+        );
+      });
+      const centerLatLng = new kakao.maps.LatLng(latitude, longitude);
+      const radius = userRadius / 1000;
+      const tempList = result.filter((item) => {
+        const lat = item.y;
+        const lng = item.x;
+        const distance = calculateDistance(
+          centerLatLng.getLat(),
+          centerLatLng.getLng(),
+          lat,
+          lng
+        );
+        return distance <= radius; // 해당 주소가 원의 반지름 내에 있는지 확인
+      });
+      const nearbyAddresses = [];
+      for (var i = 0; i < 15; i++) {
+        let cityName = tempList[i].address_name
+          .split(" ")
+          .filter((item) => item !== "")
+          .slice(0, 3)
+          .join(" ");
+        if (!nearbyAddresses.includes(cityName)) {
+          nearbyAddresses.push(cityName);
         }
-      },
-      options
-    );
+      }
+      await setNearLocation(nearbyAddresses);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
-
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
     //거리계산 메소드
-    const R = 6371; // 지구의 반지름 (단위: km)
-
+    const R = 6371; // 지구 반지름 (단위: km)
     const dLat = ((lat2 - lat1) * Math.PI) / 180;
     const dLon = ((lon2 - lon1) * Math.PI) / 180;
-
     const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos((lat1 * Math.PI) / 180) *
@@ -133,7 +130,6 @@ const Location = () => {
 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const distance = R * c;
-
     return distance; // 두 좌표 간의 거리를 반환 (단위: km)
   };
   const locationError = () => {
@@ -143,7 +139,15 @@ const Location = () => {
       longitude: 126.570667,
     });
   };
-
+  const LocationChange = (address) => {
+    console.log(address);
+    setUserLocation({ location: address });
+  };
+  const LocationConfirm = (event) => {
+    event.preventDefault();
+    console.log(userLocation);
+    //위치 업로드 함수 및 페이지 이동 함수
+  };
   return (
     <div>
       <HeaderV2 ID={""} />
@@ -151,9 +155,27 @@ const Location = () => {
       <div className="Location">
         <div id="map" className="Map"></div>
         <div>
-          {userLocation.location}
+          <p className="currentL">{userLocation.location}</p>
+          <p className="warn">
+            주변 위치로 재설정하고 싶으시다면 목록에서 선택해주세요
+          </p>
           <br />
-          근처 보기
+          {nearLocation.map((address, index) => (
+            <div key={index}>
+              <hr />
+              <button
+                className="addressBTN"
+                onClick={() => LocationChange(address)}
+              >
+                {address}
+              </button>
+            </div>
+          ))}
+          <hr />
+          <br />
+          <button className="LocationConfirm" onClick={LocationConfirm}>
+            확인
+          </button>
         </div>
       </div>
     </div>
