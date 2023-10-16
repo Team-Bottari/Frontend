8ca170b9eb1ea8e2e1a35a1bde31b09caf681159
kimeunseo58 <alan0221@naver.com>
2023-10-16 14:01:21 +0900

    Chat_2 Update

--- src/pages/chatting/Chatting.js
+++ src/pages/chatting/Chatting.js
@@ -5,8 +5,8 @@ import axios from "axios";
 const Chatting = (props) => {
   const user = props.chatData;
   const scrollContainerRef = useRef(null); //스크롤 고정용
-  const [position, setPosition] = useState(0); //스크롤 위치 가져오기용
-  //const [chatIndex, setChatIndex] = useState(0);
+  const [position, setPosition] = useState(0); //스크롤 위치
+  const [pastChat, setPastChat] = useState([]);
   var chatIndex_start = 0;
   var chatIndex_end = 0; // useState훅으로 바꿔주기
   var socket = new WebSocket(
@@ -14,38 +14,6 @@ const Chatting = (props) => {
       (user.host_id % 10) + (user.client_id % 10) + 1
     }/chatting-socket/${user.posting_id}/${user.host_id}/${user.client_id}`
   );
-  useEffect(() => {
-    socket.onmessage = function (event) {
-      console.log("메시지 수신_data:", event.data);
-      const jsonObject = JSON.parse(event.data);
-      //초기 pastChat 저장 필요.
-
-      chatIndex_start = jsonObject.index - 20;
-      chatIndex_end = jsonObject.index - 10;
-      //setChatIndex(jsonObject.index);
-      // 과거 채팅 호출 시 필요한 start_index값 저장
-    };
-  }, []);
-
-  const [pastChat, setPastChat] = useState([
-    //시간 순서대로 배열 필요
-    {
-      time: "10:11",
-      text: user.client_id,
-      chatUser: ".",
-    },
-  ]);
-  useEffect(() => {
-    socket.onopen = (event) => {
-      console.log("WebSocket connection opened:", event);
-    };
-    return () => {
-      socket.onclose = function (event) {
-        console.log("WebSocket connection closed:", event);
-      };
-    };
-  }, []);
-
   useEffect(() => {
     if (scrollContainerRef.current) {
       scrollContainerRef.current.scrollTop =
@@ -61,18 +29,55 @@ const Chatting = (props) => {
       scrollContainerRef.current.removeEventListener("scroll", isTop); // 언마운트될 때 리스너 제거
     };
   }, []);
+  useEffect(() => {
+    socket.onmessage = function (event) {
+      //console.log("data:", event.data);
+      const jsonString = event.data.replace(/'/g, '"');
+      const jsonStringModified = jsonString.replace(/True/g, "true");
+      //console.log(jsonStringModified);
+      const jsonObject = JSON.parse(jsonStringModified);
+      let flag = 0;
+      if (jsonObject.host_visible === false && user.id == user.host_id) flag++;
+      else if (jsonObject.client_visible === false && user.id == user.client_id)
+        flag++;
+      else {
+        setPastChat((prev) => {
+          const newChat = [...prev, jsonObject];
+          newChat.sort((a, b) => a.index - b.index);
+          return newChat;
+        });
 
-  //채팅 스크롤이 최상단에 위치했을 때, 추가적으로 메시지를 10개씩 불러온다.
-  // 시간 최신 순 : 30개면 29 - 20옴.
-  //API 주소 채팅 주소와 연동해서 보내기. ex) 13과 14의 경우 30008처럼 다른 url 사용해야함. 채팅은 다른 서버에서 관리중
+        chatIndex_start = jsonObject.index - 20;
+        chatIndex_end = jsonObject.index - 10;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.onopen = (event) => {
+      console.log("WebSocket connection opened:", event);
+    };
+    return () => {
+      socket.onclose = function (event) {
+        console.log("WebSocket connection closed:", event);
+      };
+    };
+  }, []);
+
   const handleNewMessage = (message) => {
     //새 메시지 도착 시 처리하는 콜백 함수
-    setPastChat((prevChat) => [...prevChat, message]);
+    setPastChat((prev) => {
+      const newChat = [...prev, message];
+      newChat.sort((a, b) => a.index - b.index);
+      return newChat;
+    });
   };
   const newChatUpdate = (event) => {
     event.preventDefault();
     const input = document.getElementById("Chat");
     const text = input.value;
+    const host_visible = true;
+    const client_visible = true;
     const date = new Date();
     let year = date.getFullYear();
     let day = date.getDate();
@@ -80,23 +85,20 @@ const Chatting = (props) => {
     let month = date.getMonth() + 1;
     let minutes = date.getMinutes().toString().padStart(2, "0");
     const time = `${year}-${month}-${day}-${hours}-${minutes}`;
-    const chatUser = user.nickName; // 사용자 이름을 저장
-    //console.log(user.nickName, chatUser);
     const message = {
       text,
       time,
-      chatUser: "TEST",
+      chatUser: user.chatUser,
+      host_visible,
+      client_visible,
     };
     if (text !== "") {
       socket.send(JSON.stringify(message));
-      console.log(message);
-      setPastChat((prevChat) => [...prevChat, message]);
-      //index도 받아야함!
+      //console.log(JSON.stringify(message));
     }
     input.value = ""; // 채팅 입력란을 초기화
   };
   const getOldChat = async (event) => {
-    //console.log(chatIndex);
     await axios
       .get(
         `http://wisixicidi.iptime.org:3000${
@@ -113,9 +115,9 @@ const Chatting = (props) => {
         }
       )
       .then((response) => {
-        console.log(response); //리스폰스 pastchat에 저장하기
+        console.log(response.data.messages); //리스폰스 pastchat에 저장하기
         setPastChat((prev) => {
-          const newChat = [...prev, response];
+          const newChat = [...prev, ...response.data.messages];
           newChat.sort((a, b) => a.index - b.index);
           return newChat;
         });
@@ -128,19 +130,37 @@ const Chatting = (props) => {
         console.log(err);
       });
   };
+  const deletRoomm = async (event) => {
+    await axios
+      .post(
+        "http://wisixicidi.iptime.org:30000/api/v1.0.0/chatting/exiting-chatting",
+        {
+          posting_id: user.posting_id,
+          host_id: user.host_id.toString(),
+          client_id: user.client_id.toString(),
+          request_client: "13",
+        }
+      )
+      .then((response) => {
+        console.log("삭제 성공:", response);
+      })
+      .catch((error) => {
+        console.error("삭제 실패:", error);
+      });
+  };
   return (
     <div className="Chatting">
       <div className="pastChat" ref={scrollContainerRef}>
         {pastChat.map((chat, index) => (
           <div key={index}>
-            {chat.chatUser !== user.nickName && (
+            {chat.text !== "" && chat.chatUser !== user.chatUser && (
               <div className="leftChat">
                 <span className="ChatUser">{chat.chatUser}</span>
                 <div className="ChatTextL">{chat.text}</div>
                 <span className="ChatTime">{chat.time}</span>
               </div>
             )}
-            {chat.chatUser === "TEST" && (
+            {chat.text !== "" && chat.chatUser === user.chatUser && (
               <div className="rightChat">
                 <span className="ChatTime">{chat.time}</span>
                 <div className="ChatTextR">{chat.text}</div>
@@ -157,18 +177,18 @@ const Chatting = (props) => {
         <button className="chatButton" onClick={newChatUpdate}>
           전송
         </button>
-        <button onClick={getOldChat}>불러오기</button>
+        <button onClick={deletRoomm}>삭제</button>
       </div>
     </div>
   );
 };
 Chatting.defaultProps = {
   user: {
-    email: "",
-    nickName: "",
+    chatUser: "string",
+    id: 14,
     posting_id: "string",
     host_id: 13,
-    client_id: 15,
+    client_id: 14,
   },
 };
 export default Chatting;
